c86440299659d9e0b0515924942e17a1d20cca96
Andrea Bianchi <bianan96@gmail.com>
2022-10-27 20:41:13 +0200

    Create placeholder for TweetFeed component

---
+++ src/components/TweetCard/TweetCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import * as style from './TweetCard.module.css'
+
+const fakeData = {
+  id: '2464325',
+  text: 'Il testo del tweet',
+  author: {
+    id: '2445651',
+    name: 'Dan Caccia',
+    username: '@dancaccia'
+  }
+}
+
+
+const TweetCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TweetCard
