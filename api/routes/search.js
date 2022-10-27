349ae3386005828e21b36b7cccabe6b0ccf68eba
Luca Bassi <luca.bassi14@studio.unibo.it>
2022-10-27 19:28:58 +0200

    Add API search endpoint

--- api/routes/search.js
+++ api/routes/search.js
@@ -2,8 +2,27 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/:query', async (req, res) => {
-	const tweet = await twitterClient.tweets.tweetsRecentSearch({'query': req.params.query});
-	res.status(200).send(tweet);
+	try {
+		const tweetsData = await twitterClient.tweets.tweetsRecentSearch({'query': req.params.query, 'expansions': 'author_id'});
+		let users = new Map();
+		tweetsData.includes.users.forEach((user) => {
+			users.set(user.id, {'name': user.name, 'username': user.username});
+		});
+		let tweets = [];
+		tweetsData.data.forEach((tweet) => {
+			tweets.push({
+				'id': tweet.id,
+				'text': tweet.text,
+				'author': {
+					'id': tweet.author_id,
+					...users.get(tweet.author_id)
+				}
+			});
+		});
+		res.status(200).send(tweets);
+	} catch (err) {
+		res.status(400).send(err.error);
+	}
 });
 
 module.exports = router;
--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -2,7 +2,15 @@ describe('Check if search works', () => {
 	it('passes', () => {
 		cy.request('GET', '/api/search/test').then((response) => {
 			expect(response.status).to.eq(200);
-			expect(response.body.meta.result_count).to.eq(response.body.data.length);
+			expect(response.body).length.to.be.greaterThan(1);
+			expect(response.body[0]).to.have.property('id');
+		});
+		cy.request({
+			method: 'GET',
+			url: '/api/search/"',
+			failOnStatusCode: false
+		}).then((response) => {
+			expect(response.status).to.eq(400);
 		});
 	});
 });
