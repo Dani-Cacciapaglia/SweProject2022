42e9b4ff513d30a200db06808d7e71d36a31c89a
Andrea Bianchi <bianan96@gmail.com>
2022-10-28 22:32:36 +0200

    Add Search component to main page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,21 @@
-import * as React from "react";
+import * as React from 'react';
 
 import * as style from './index.module.css';
 
-import { Header } from '../components/Header/Header';
+import Header from '../components/Header';
+import Search from '../components/Search';
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
 
 const IndexPage = () => {
   return (
-    <main>
+    <main className={style.main}>
       <Header />
+      <Search placeholderText="Cerca tweet..." />
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head = () => <title>Home Page</title>
-
+export const Head = () => <title>Home Page</title>;
