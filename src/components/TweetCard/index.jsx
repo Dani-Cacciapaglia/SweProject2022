311fc8e2ccb2c9db355c2b429df02b556afccf15
Andrea Bianchi <bianan96@gmail.com>
2022-10-29 11:54:54 +0200

    remove mocks

--- src/components/TweetCard/index.jsx
+++ src/components/TweetCard/index.jsx
@@ -4,17 +4,7 @@ import * as style from './TweetCard.module.css';
 
 import Logo from '../../assets/twitter-logo.jpg';
 
-const fakeData = {
-  id: '2464325',
-  text: 'Il testo del tweet',
-  author: {
-    id: '2445651',
-    name: 'Dan Caccia',
-    username: '@dancaccia',
-  },
-};
-
-const TweetCard = () => {
+const TweetCard = ({ tweetData }) => {
   return (
     <div className={style.wrapper}>
       <div className={style.profileWrapper}>
@@ -24,12 +14,12 @@ const TweetCard = () => {
           alt="immagine profilo"
         />
         <p className={style.profileText}>
-          <span className={style.profileName}>{fakeData.author.name}</span>
+          <span className={style.profileName}>{tweetData.author.name}</span>
           {` `}
-          {fakeData.author.username}
+          {tweetData.author.username}
         </p>
       </div>
-      <div className={style.contentWrapper}>{fakeData.text}</div>
+      <div className={style.contentWrapper}>{tweetData.text}</div>
     </div>
   );
 };
