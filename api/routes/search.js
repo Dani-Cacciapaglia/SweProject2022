c9aeac5c6b0e210c8a24467ef8dbc4097677fdc4
Luca Bassi <luca.bassi14@studio.unibo.it>
2022-10-31 11:20:59 +0100

    Return created_at tweet data

--- api/routes/search.js
+++ api/routes/search.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/:query', async (req, res) => {
 	try {
-		const tweetsData = await twitterClient.tweets.tweetsRecentSearch({'query': req.params.query, 'expansions': 'author_id', 'user.fields': 'profile_image_url'});
+		const tweetsData = await twitterClient.tweets.tweetsRecentSearch({'query': req.params.query, 'tweet.fields': 'created_at', 'expansions': 'author_id', 'user.fields': 'profile_image_url'});
 		let users = new Map();
 		tweetsData.includes.users.forEach((user) => {
 			users.set(user.id, {'name': user.name, 'username': user.username, 'profile_image_url': user.profile_image_url});
@@ -13,6 +13,7 @@ router.get('/:query', async (req, res) => {
 			tweets.push({
 				'id': tweet.id,
 				'text': tweet.text,
+				'created_at': tweet.created_at,
 				'author': {
 					'id': tweet.author_id,
 					...users.get(tweet.author_id)
