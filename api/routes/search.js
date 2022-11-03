96ca77907812e1b06d6badffd5d1119b9d729c58
Luca Bassi <luca.bassi14@studio.unibo.it>
2022-11-03 16:33:27 +0100

    Add location to search API

--- api/routes/search.js
+++ api/routes/search.js
@@ -1,13 +1,25 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 router.get('/:query', async (req, res) => {
 	try {
-		const tweetsData = await twitterClient.tweets.tweetsRecentSearch({'query': req.params.query, 'tweet.fields': 'created_at', 'expansions': 'author_id', 'user.fields': 'profile_image_url'});
+		const tweetsData = await twitterClient.tweets.tweetsRecentSearch({
+			'query': req.params.query,
+			'tweet.fields': 'created_at',
+			'expansions': 'author_id,geo.place_id',
+			'user.fields': 'profile_image_url',
+			'place.fields': 'geo'
+		});
 		let users = new Map();
 		tweetsData.includes.users.forEach((user) => {
 			users.set(user.id, {'name': user.name, 'username': user.username, 'profile_image_url': user.profile_image_url});
 		});
+		let places = new Map();
+		if (tweetsData.includes.places) {
+			tweetsData.includes.places.forEach((place) => {
+				places.set(place.id, {'full_name': place.full_name, 'geo': place.geo});
+			});
+		}
 		let tweets = [];
 		tweetsData.data.forEach((tweet) => {
 			tweets.push({
@@ -19,6 +31,12 @@ router.get('/:query', async (req, res) => {
 					...users.get(tweet.author_id)
 				}
 			});
+			if (tweet.geo) {
+				tweets[tweets.length - 1]['place'] = {
+					'id': tweet.geo.place_id,
+					...places.get(tweet.geo.place_id)
+				};
+			}
 		});
 		res.status(200).send(tweets);
 	} catch (err) {
