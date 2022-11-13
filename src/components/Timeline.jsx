3f6c8b74b0d2832ffa84728ee7cf38b88119b3b8
Andrea Bianchi <bianan96@gmail.com>
2022-11-13 15:54:39 +0100

    Create components for tweets timeline

---
+++ src/components/Timeline.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import TweetCard from './TweetCard';
+
+import { SearchContext } from '../hooks/SearchContext';
+
+export const Timeline = ({ loadMore }) => {
+  const { result } = useContext(SearchContext);
+
+  return (
+    <>
+      <section className="flex flex-col max-w-prose gap-4 p-4 mx-auto">
+        {result.map((item, index) => (
+          <TweetCard key={index} tweetData={item} />
+        ))}
+      </section>
+      {result[result.length - 1].next_token && (
+        <button
+          className="bg-sky-300 hover:bg-sky-400 rounded p-2"
+          onClick={(e) => {
+            loadMore(e, result[result.length - 1].next_token);
+          }}
+        >
+          Carica altro
+        </button>
+      )}
+    </>
+  );
+};
