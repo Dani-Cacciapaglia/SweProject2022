9e7da6f9064b9eeb19892851c8db39522b2794bf
Luca Bassi <luca.bassi14@studio.unibo.it>
2022-11-25 15:19:50 +0100

    Revert "Added chessboard tab and divided map and feed from chess part"
    This reverts commit 8fe21d3a4f242a9cd1082acfe4e4bd98e0530796.

--- src/components/TweetFeed.jsx
+++ src/components/TweetFeed.jsx
@@ -14,43 +14,26 @@ const TweetFeed = ({ loadMore }) => {
         <Tab.Group>
           <Tab.List className="flex flex-row justify-around w-full max-w-prose">
             <Tab className="border-b-2 hover:font-bold ui-selected:font-bold ui-selected:border-b-4 ui-selected:border-sky-300">
-              Reazione
+              Timeline
             </Tab>
             <Tab
-              className="border-b-2 disabled:text-neutral-700 enabled:hover:font-bold ui-selected:font-bold ui-selected:border-b-4 ui-selected:border-sky-300">
-              Scacchi
+              className="border-b-2 disabled:text-neutral-700 enabled:hover:font-bold ui-selected:font-bold ui-selected:border-b-4 ui-selected:border-sky-300"
+              disabled={
+                result.filter((item) => {
+                  return item.place;
+                }).length === 0
+              }
+            >
+              Mappa
             </Tab>
           </Tab.List>
           <Tab.Panels>
             <Tab.Panel className="flex flex-col items-center">
-              <Tab.Group>
-                <Tab.List className="flex flex-row justify-around w-full max-w-prose">
-                  <Tab className="border-b-2 hover:font-bold ui-selected:font-bold ui-selected:border-b-4 ui-selected:border-sky-300">
-                    Timeline
-                  </Tab>
-                  <Tab
-                    className="border-b-2 disabled:text-neutral-700 enabled:hover:font-bold ui-selected:font-bold ui-selected:border-b-4 ui-selected:border-sky-300"
-                    disabled={
-                     result.filter((item) => {
-                       return item.place;
-                      }).length === 0
-                   }>
-                     Mappa
-                   </Tab>
-                </Tab.List>
-               <Tab.Panels>
-                  <Tab.Panel className="flex flex-col items-center">
-                   <Timeline loadMore={loadMore} />
-                  </Tab.Panel>
-                  <Tab.Panel>
-                    <Map />
-                  </Tab.Panel>
-               </Tab.Panels>
-              </Tab.Group>
+              <Timeline loadMore={loadMore} />
             </Tab.Panel>
-              <Tab.Panel>
-                //inserimento scacchiera qui
-              </Tab.Panel>
+            <Tab.Panel>
+              <Map />
+            </Tab.Panel>
           </Tab.Panels>
         </Tab.Group>
       )}
