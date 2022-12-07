4153b5c2c719a1f9d27a3e9ce90de16674448da6
Luca Bassi <luca.bassi14@studio.unibo.it>
2022-12-07 20:10:25 +0100

    Add fantacitorio API tests

---
+++ cypress/e2e/fantacitorio.cy.js
@@ -0,0 +1,10 @@
+describe('Check if fantacitorio works', () => {
+	it('passes', () => {
+		cy.request('GET', '/api/fantacitorio/scores').then((response) => {
+			expect(response.status).to.eq(200);
+			expect(response.body).length.to.be.greaterThan(3);
+			expect(response.body[0]).to.have.property('Bagnai');
+			expect(response.body[0]['Bagnai']).to.eq(45);
+		});
+	});
+});
