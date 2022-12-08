7f3aeb019d7b94c5b26cf04e2531f9ad71bc01bc
Luca Bassi <luca.bassi14@studio.unibo.it>
2022-12-08 09:31:55 +0100

    Fix load more teams button and load first teams automatically

--- src/components/FantacitorioTeams.jsx
+++ src/components/FantacitorioTeams.jsx
@@ -7,17 +7,20 @@ import './style.css';
 
 export const FantacitorioTeams = () => {
   const [teams, setTeams] = useState([]);
-  const searchTeams = async () => {
-    const res = await axios.get(
-      `${window.$apiUrl}/search/${encodeURIComponent(
-        '#fantacitorio has:media'
-      )}`
-    );
-    setTeams(res.data);
-    console.log(teams);
-  };
+  const [next_token, setNextToken] = useState('');
 
-  // useEffect(() => searchTeams, []);
+  useEffect(() => {
+    async function loadTeams() {
+      const res = await axios.get(
+        `${window.$apiUrl}/search/${encodeURIComponent(
+          '#fantacitorio has:media'
+        )}`,
+        { params: { next_token: next_token } }
+      );
+      setTeams((t) => t.concat(res.data));
+    }
+    loadTeams();
+  }, [next_token]);
 
   return (
     <div className="fantacitorio-teams-div">
@@ -31,14 +34,16 @@ export const FantacitorioTeams = () => {
             )
         )}
       </section>
-      <button
-        className="btn"
-        onClick={(e) => {
-          searchTeams();
-        }}
-      >
-        Carica squadre
-      </button>
+      {teams.length > 0 && teams[teams.length - 1].next_token && (
+        <button
+          className="btn"
+          onClick={(e) => {
+            setNextToken(teams[teams.length - 1].next_token);
+          }}
+        >
+          Carica squadre
+        </button>
+      )}
     </div>
   );
 };
